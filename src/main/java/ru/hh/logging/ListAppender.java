08a09f4c56c9c4e0f281b455730918a4effb61a2
andrew <an.sumin@hh.ru>
2018-09-07 15:55:22 +0300

    HH-81885 move ListAppender from logback-utils repo

---
+++ src/main/java/ru/hh/logging/ListAppender.java
@@ -0,0 +1,10 @@
+package ru.hh.logging;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class ListAppender extends ch.qos.logback.core.read.ListAppender<ILoggingEvent> {
+
+  public synchronized String getLogLineBySubstring(String substring) {
+    return list.stream().map(ILoggingEvent::getFormattedMessage).filter(m -> m.contains(substring)).findFirst().orElse(null);
+  }
+}
